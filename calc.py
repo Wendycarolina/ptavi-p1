388dc4eafd1e6090f34d82c6356bfc40467b77b2
wendy carolina iza gualiche <wendy@zeta40.aulas.gsyc.es>
2015-09-21 10:59:38 +0200

    calculadora

--- calc.py
+++ calc.py
@@ -2,9 +2,26 @@
 # -*- coding: utf-8 -*-
 
 # Vamos a programar una calculadora
+#git commit -m "..." calc.py
+
+import sys
 
 def main():
     pass
 
+def suma(sumando1, sumando2):
+	return sumando1 + sumando2 
+
+def multiplica(numero1, numero2)
+	return numero1*numero2
+
+def division(divide1, divide2)
+	return divide1 / divide2
+		
+
 if __name__ == "__main__":
-    main()
+	try:
+		operando1 = int(sys.argv[2])
+		operando = int(sys.argv[3])
+	except ValueError:
+		print(suma(operando1,operando2))
