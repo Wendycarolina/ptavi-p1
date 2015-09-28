bcc1638914c47bd800bc6e4f72c09784ab0b4c19
Wendycarolina <carolina.iza.g@gmail.com>
2015-09-28 20:15:51 +0200

    Update calcoo.py

--- calcoo.py
+++ calcoo.py
@@ -1,22 +1,32 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-import sys
 
 class Calculadora():
-	def __init__(self,numero1,numero2)
+	def __init__(self,numero1,numero2):
 		self.numero1 = numero1
 		self.numero2 = numero2
 
 	def suma(self,sumando1, sumando2):
 		return sumando1 + sumando2 
-
+		
 	def resta(self,numero1, numero2):
 		return numero1 - numero2
+		
 
 if __name__ == "__main__":
-	x = Calculadora()
-	try:
 	
-		x.numero1 = int(sys.argv[1])
+	try:
+		numero1 = float(sys.argv[1])
 		operacion = str(sys.argv[2])
-		x.numero2 = int(sys.argv[3])
+		numero2 = float(sys.argv[3])
+		x = Calculadora(numero1, numero2)
+
+	except ValueError:
+		sys.exit("Error: Non numerical parameters")
+
+	if operacion == "suma":
+		print(x.suma(numero1,numero2))
+	elif operacion == "resta":
+		print(x.resta(numero1,numero2))
+	else:
+		print("Operación incorrecta")
